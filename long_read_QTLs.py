09e492fa9594ca0b006624a09ff58b844af5c51f
Abraham Moller <mollerabg@biowulf.nih.gov>
2025-06-11 02:42:45 -0400

    Added MAF/methylation rate filters to long read QTL script

--- long_read_QTLs.py
+++ long_read_QTLs.py
@@ -19,6 +19,10 @@ def parse_args():
     parser.add_argument("--metadata", required=True, help="Path to the metadata file.")
     parser.add_argument("--output", required=True, help="Output file destination for results.")
     parser.add_argument("--window_size", type=int, default=500000, help="Window size for defining gene regions (default: 500000).")
+    # parser.add_argument("--minimum_genotypes",type=int, default=3, help="Minimum number of variant genotypes to run regression (default: 3).")
+    parser.add_argument("--per_haplotype_missing_methylation_rate", default=0.95, help="Proportion of methylation calls missing per haplotype to consider for corresponding MAF exclusion (default: 0.95).")
+    parser.add_argument("--per_haplotype_MAF", default=0.05, help="Minimum minor allele frequency per haplotype to run regression in the case of one haplotype with data (default: 0.05).")
+    parser.add_argument("--overall_MAF", default=0.05, help="Minimum minor allele frequency for both haplotypes to run regression (default: 0.05).")
     return parser.parse_args()
 
 def main():
@@ -94,21 +98,56 @@ def main():
         for outcome in outcomes:
             for predictor in predictors:
                 try:
-                    formula = f"Q('{outcome}') ~ {predictor} + {' + '.join(covariates_list)}"
-                    model = smf.ols(formula, data=merged_data).fit()
+                    # check for overall minor allele frequency (lesser of two absent or present frequencies)
+                    haplotype_outcome_predictor=merged_data[['HAPLOTYPE',outcome,predictor]]
+                    # reference frequency is proportion of 0 genotypes
+                    ref_variant_frequency=sum(haplotype_outcome_predictor[predictor]==0)/len(haplotype_outcome_predictor[predictor])
+                    # per haplotype
+                    ref_variant_frequency_H1=sum(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H1'][predictor]==0)/len(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H1'][predictor])
+                    ref_variant_frequency_H2=sum(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H2'][predictor]==0)/len(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H2'][predictor])
+                    # alternate frequency is proportion of 1 genotypes
+                    alt_variant_frequency=sum(haplotype_outcome_predictor[predictor]==1)/len(haplotype_outcome_predictor[predictor])
+                    # per haplotype
+                    alt_variant_frequency_H1=sum(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H1'][predictor]==1)/len(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H1'][predictor])
+                    alt_variant_frequency_H2=sum(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H2'][predictor]==1)/len(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H2'][predictor])
+                    # minor allele frequency is least of either reference or alternate frequencies
+                    overall_minor_allele_frequency=min(ref_variant_frequency,alt_variant_frequency)
+                    # MAF per haplotype
+                    minor_allele_frequency_H1=min(ref_variant_frequency_H1,alt_variant_frequency_H1)
+                    minor_allele_frequency_H2=min(ref_variant_frequency_H2,alt_variant_frequency_H2)
+                    # get single haplotype minor allele frequency
+                    # check for data with missing methylation for one haplotype
+                    proportion_meth_H1_missing=haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H1'][outcome].isna().sum()/len(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H1'][outcome])
+                    proportion_meth_H2_missing=haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H2'][outcome].isna().sum()/len(haplotype_outcome_predictor[haplotype_outcome_predictor['HAPLOTYPE']=='H2'][outcome])
                     
-                    results.append({
-                        'gene': gene_of_interest,
-                        'window_size': args.window_size,
-                        'outcome': outcome,
-                        'predictor': predictor,
-                        'beta': model.params.get(predictor, np.nan),
-                        'std_err': model.bse.get(predictor, np.nan),
-                        'r2': model.rsquared,
-                        'p_value': model.pvalues.get(predictor, np.nan),
-                        'N': model.nobs,
-                        'predictor_freq': merged_data[predictor].mean(skipna=True)
-                    })
+                    if (overall_minor_allele_frequency > args.overall_MAF and proportion_meth_H1_missing < args.per_haplotype_missing_methylation_rate and proportion_meth_H2_missing < args.per_haplotype_missing_methylation_rate) or (proportion_meth_H1_missing > args.per_haplotype_missing_methylation_rate and minor_allele_frequency_H2 > args.per_haplotype_MAF) or (proportion_meth_H2_missing > args.per_haplotype_missing_methylation_rate and minor_allele_frequency_H1 > args.per_haplotype_MAF):
+                        
+                        formula = f"Q('{outcome}') ~ {predictor} + {' + '.join(covariates_list)}"
+                        model = smf.ols(formula, data=merged_data).fit()
+                        
+                        results.append({
+                            'gene': gene_of_interest,
+                            'window_size': args.window_size,
+                            'outcome': outcome,
+                            'predictor': predictor,
+                            'beta': model.params.get(predictor, np.nan),
+                            'std_err': model.bse.get(predictor, np.nan),
+                            'r2': model.rsquared,
+                            'p_value': model.pvalues.get(predictor, np.nan),
+                            'N': model.nobs,
+                            'predictor_freq': merged_data[predictor].mean(skipna=True),
+                            # print intercept
+                            'intercept': model.params.Intercept,
+                            # print overall allele frequency
+                            'overall_MAF': overall_minor_allele_frequency,
+                            # print minor allele frequency for haplotype 1 (H1)
+                            'H1_MAF': minor_allele_frequency_H1,
+                            # print minor allele frequency for haplotype 2 (H2)
+                            'H2_MAF': minor_allele_frequency_H2,
+                            # print missing methylation per haplotype
+                            'H1_missing_meth': proportion_meth_H1_missing,
+                            'H2_missing_meth': proportion_meth_H2_missing
+                        })
                     
                     # print(model.summary())
                 except Exception as e:
